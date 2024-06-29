3c6633d772d9d9d5865dd08213794180efd789af
10xtarun <10xtarun@gmail.com>
2024-06-29 12:58:15 +0530

    mongodb connection logic added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,46 @@
 const express = require("express")
 const dotenv = require("dotenv")
-dotenv.config()
+const mongoose = require("mongoose")
 
+dotenv.config()
 const app = express()
 
-app.get("/", (req, res) => {
-    return res.send("Greetings! Welcome to Job Tracker API.")
-})
+return Promise.resolve()
+    .then(() => {
+        // mongodb init
+        return mongoose.connect(process.env.MONGO_URI, {})
+            .then(connection => {
+                console.log("mongo db connection established")
+                // console.log("Connection: ", connection.connection.db)
+            })
+    })
+    .then(() => {
+        // add middlewares
+        app.use(express.json())
+        app.use(express.urlencoded({ extended: true }))
 
-app.listen(process.env.PORT, () => {
-    console.log(`job tracker api is running on port ${process.env.PORT}`)
-})
+        // custom middleware
+        app.use((req, res, next) => {
+            console.log(`API Logging --- METHOD: ${req.method} URL: ${req.originalUrl} BODY: ${req.body ? JSON.stringify(req.body) : "Empty"}`)
+            next()
+        })
+    })
+    .then(() => {
+        app.get("/", (req, res) => {
+            return res.send("Greetings! Welcome to Job Tracker API.")
+        })
+
+        app.post("/auth/register", (req, res) => {
+            return res.send("Registered successfully.")
+        })
+    })
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`job tracker api is running on port ${process.env.PORT}`)
+        })
+    })
+    .catch(error => {
+        console.log("job tracker api start error")
+        console.log("Error: ", error)
+        process.exit(1)
+    })
